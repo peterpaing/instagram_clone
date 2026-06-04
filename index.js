88f2329b6feae56c6c77e143131b2167ae0858d3
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-04 23:17:07 +0630

    Implement dynamic post rendering: add renderPosts function to display user posts with likes and comments

--- index.js
+++ index.js
@@ -28,3 +28,42 @@ const posts = [
     }
 ]
 
+const postsContainer = document.querySelector(".posts")
+
+function renderPosts() {
+    let postHTML = ""
+
+    for (const post of posts) {
+        postHTML += `
+        <section class="profile">
+            <div class="profile-header">
+                <img src="${post.avatar}" alt="Avatar Image" class="poster-profile">
+                <p>
+                    ${post.name}
+                    <span class="poster-location">${post.location}</span>
+                </p>
+            </div>
+
+            <div class="profile-content">
+                <img src="${post.post}" alt="Post Image">
+            </div>
+
+            <div class="profile-footer">
+                <div class="react-container">
+                    <img src="images/icon-heart.png" alt="Heart Icon">
+                    <img src="images/icon-comment.png" alt="Comment Icon">
+                    <img src="images/icon-dm.png" alt="Share Icon">
+                </div>
+
+                <div class="likes-comments">
+                    <p>${post.likes} likes</p>
+                    <p>${post.username}<span> ${post.comment}</span></p>
+                </div>
+            </div>
+        </section>
+        `
+    }
+
+    postsContainer.innerHTML = postHTML
+}
+renderPosts()
