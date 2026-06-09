bcd71557710aab390e0b5e274103d578be594544
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-09 17:45:12 +0630

    separate another file for data and add uuid

---
+++ data.js
@@ -0,0 +1,32 @@
+export const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21,
+        uuid :'c208da18-fe66-4627-b7cf-55dce08775aa' ,
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4 ,
+        uuid :'8cbd1195-3fbb-4628-8df2-a8eb93c545aa'
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152,
+        uuid : 'df5138b8-fc38-42f5-9392-ad82212b8edc'
+    }
+]
